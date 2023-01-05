0dda272491b6fce346e5cc3abcd77b69bbd63943
YHDF <unes.houdaifa@gmail.com>
2023-01-05 01:21:06 +0100

    commit

---
+++ task-manager/frontent-graphql/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+
+export default class App extends React.Component {
+
+
+
+  componentDidMount() {
+    const client = new ApolloClient({
+      uri: 'http://localhost:5000',
+      cache: new InMemoryCache(),
+    });
+
+
+    client
+      .query({
+        query: gql`
+        query getActions {
+          actions {
+            target_id
+          }
+        }
+  `,
+      })
+      .then((result) => console.log(result));
+  }
+
+
+
+  render() {
+    console.log("fdsfsdf")
+    return (
+      <div >
+        fejedbfksdbfkjdssqdsqdsqdds
+      </div>
+    );
+  }
+}
